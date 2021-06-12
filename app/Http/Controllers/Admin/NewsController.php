7ea7c473a3a5075b470b8767b106cebee715b93a
Krupnov <kruevg2@gmail.com>
2021-06-12 11:42:48 +0300

    Выполнено ДЗ урока 6

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Support\Str;
+use App\Models\News;
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller  
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+        $newsList = News::with('category')
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+    
+        return view('admin.news.index', [ 
+            'newsList' => $newsList
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('admin.news.create', [  
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required']
+        ]);
+
+        $fields = $request->only('category_id', 'title', 'image', 'description', 'status');
+        $fields['slug'] = Str::slug($fields['title']);
+        $news = News::create($fields);
+        if ($news) {
+            return redirect()->route('news.index');
+        }
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(News $news)
+    {
+        return view('admin.news.show', [
+            'news' => $news
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(News $news)
+    {
+        $categories = Category::all();
+        return view('admin.news.edit', [ 
+            'news' => $news,
+            'categories' => $categories 
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, News $news)
+    {
+        $request->validate([
+            'title' => ['required']
+        ]);
+
+        $fields = $request->only('category_id', 'title', 'image', 'description', 'status');
+        $fields['slug'] = Str::slug($fields['title']);
+
+        $news = $news->fill($fields)->save();
+        if ($news) {
+            return redirect()->route('news.index');
+        }
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($news)
+    {
+        $news = News::find($news);
+        $news->delete();
+
+        return redirect()->route('news.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Http\Controllers\Form\OrdersController;
+use App\Http\Controllers\Form\ReviewsController;
+use App\Http\Controllers\Admin\OrdersController as AdminOrderController;
+use App\Http\Controllers\Admin\NewsController as AdminNewsController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\FeedbackController;
+use App\Http\Controllers\NewsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+
+
+//admin
+Route::group(['prefix' => 'admin'], function() {
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/news', AdminNewsController::class);
+    Route::resource('/review', FeedbackController::class);
+    Route::resource('/order', AdminOrderController::class);
+});
+
+//news
+Route::get('/', [NewsController::class, 'index']);
+
+Route::get('/category', [NewsController::class, 'newsCategoryShow'])
+    ->name('newsCategory');
+
+Route::get('/category/{id}/news', [NewsController::class, 'newsOutput']) 
+    ->where('id', '\d+')
+    ->name('news');
+
+Route::get('/news/{id}', [NewsController::class, 'newsShow'])
+    ->where('id', '\d+')
+    ->name('newsshow');
+
+//form
+Route::group(['prefix' => 'form'], function() {
+    Route::resource('/reviews', ReviewsController::class)
+        ->name('index', 'reviews'); 
+    Route::resource('/orders', OrdersController::class)
+        ->name('index', 'orders');
+});
+
